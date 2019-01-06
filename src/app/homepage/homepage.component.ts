e2c5f383764331f4560f3db221bf0172b73ad554
Colin Mercer <colin.mercer@topspindigital.com>
2019-01-06 15:34:23 +0000

    Prerender

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+import { KenticoClientService } from '../core/services/kentico.service';
+import { Homepage } from '../core/models/homepage';
 
 @Component({
   selector: 'app-homepage',
@@ -6,10 +12,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
+  protected ngUnsubscribe: Subject<void> = new Subject<void>();
 
-  constructor() { }
+  public error?: string;
+  homepage: Homepage
+
+  constructor(
+    private kenticoService: KenticoClientService,
+    private meta: Meta,
+    private title: Title
+  ) { }
 
   ngOnInit() {
+    this.loadData();
+    this.setTitleAndMetadata();
   }
 
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
+  private setTitleAndMetadata() {
+    this.title.setTitle('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+    this.meta.updateTag({ name: 'description', content: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' });
+    this.meta.updateTag({ property: 'og:title', content: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' });
+    this.meta.updateTag({ property: 'og:image', content: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' });
+    this.meta.updateTag({ property: 'og:site_name', content: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' });
+    this.meta.updateTag({ property: 'og:description', content: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' });
+    this.meta.updateTag({ name: 'twitter:card', content: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' });
+    this.meta.updateTag({ name: 'twitter:site', content: '@xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' });
+    this.meta.updateTag({ name: 'twitter:title', content: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' });
+    this.meta.updateTag({ name: 'twitter:description', content: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' });
+    this.meta.updateTag({ name: 'twitter:image', content: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' });
+  }
+
+  private loadData() {
+    this.kenticoService.deliveryClient
+      .items<Homepage>()
+      .type('homepage')
+      .depthParameter(0)
+      .getObservable()
+      .pipe(
+        takeUntil(this.ngUnsubscribe)
+      )
+      .subscribe(
+        response => {
+          this.homepage = response.items[0];
+          this.setTitleAndMetadata();
+          console.log('homepage', response)
+        },
+      );
+  }
 }
